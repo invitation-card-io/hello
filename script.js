3d4121828189f07405a688725c6f91bbd6a7ade6
invitation-card-io <seojmsstorage@gmail.com>
2026-03-29 18:49:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.2 });
 
 reveals.forEach(el => observer.observe(el));
+
+
+const car = document.querySelector(".car");
+const carSection = document.querySelector(".car-wrapper");
+
+window.addEventListener("scroll", () => {
+  const rect = carSection.getBoundingClientRect();
+
+  const screenHeight = window.innerHeight;
+
+  // Only animate when section is visible
+  if (rect.top < screenHeight && rect.bottom > 0) {
+
+    const progress = 1 - rect.top / screenHeight;
+
+    const maxMove = window.innerWidth * 0.6;
+
+    const moveX = Math.max(0, Math.min(progress * maxMove, maxMove));
+
+    car.style.transform = `translateX(${moveX}px)`;
+  }
+});
